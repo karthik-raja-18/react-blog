1ba5030553020732c5fc841c04e7e0b0537eef75
karthik-raja-18 <karthikraja.g2023cse@sece.ac.in>
2025-04-05 18:17:00 +0530

    Fix: updated server to use correct port and start script

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
 
-
+const cors = require('cors');
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://blog-app-rose-kappa.vercel.app/',
   credentials: true
 }));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 
 
